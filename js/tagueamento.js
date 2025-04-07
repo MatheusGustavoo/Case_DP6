3aeec66068edbc81d7b02ffb4a5539b1d264fa96
PC_DP6 <matheus.pedra@dp6.com.br>
2025-04-07 16:02:53 -0300

    Testes

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -10,4 +10,4 @@
   }); var f = d.getElementsByTagName(s)[0],
     j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
       'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-5SND65DQ')
+})(window, document, 'script', 'dataLayer', 'GTM-PKST2TBH');
