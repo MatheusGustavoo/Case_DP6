0952496568ec55b81ca2997c6c0bd025c2a76450
PC_DP6 <matheus.pedra@dp6.com.br>
2025-04-07 15:49:21 -0300

    Testes

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -3,4 +3,11 @@
 // Manager. No último caso, não é necessário implementar a tag <noscript>.
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+(function (w, d, s, l, i) {
+  w[l] = w[l] || []; w[l].push({
+    'gtm.start':
+      new Date().getTime(), event: 'gtm.js'
+  }); var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-5SND65DQ')
